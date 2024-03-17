d2a9518db17c937dd31dbe5424651d6bf1712759
Geo <geo@web-opt.com>
2024-03-17 02:49:39 -0400

    simplify

--- index.test.js
+++
@@ -1,8 +0,0 @@
-import svelteMounter from './index';
-import {describe, expect, it} from '@jest/globals';
-
-describe('describe svelteMounter', () => {
-    it('should be function', () => {
-        expect(typeof svelteMounter).toBe('ffunction')
-    });
-});
